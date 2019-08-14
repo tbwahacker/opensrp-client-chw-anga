5a0aeb980e17ab1b96dcf14eb3b4b79eaad9af47
Denis Rwelamila <rwelamila.jr@gmail.com>
2019-08-14 13:08:07 +0300

    codacy

---
+++ opensrp-chw-malaria/src/test/java/org/smartregister/chw/malaria/provider/MalariaRegisterProviderTest.java
@@ -0,0 +1,29 @@
+package org.smartregister.chw.malaria.provider;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+public class MalariaRegisterProviderTest {
+    @Mock
+    private MalariaRegisterProvider malariaRegisterProvider;
+
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void updateClients() {
+        Assert.assertNull(malariaRegisterProvider.updateClients(null, null, null, null));
+    }
+
+
+    @Test
+    public void newFormLauncher() {
+        Assert.assertNull(malariaRegisterProvider.newFormLauncher(null, null, null));
+    }
+}
