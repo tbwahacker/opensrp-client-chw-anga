d65be4e8a7d166438f15596bbfb197a8b65033fc
Denis Rwelamila <rwelamila.jr@gmail.com>
2019-09-02 13:23:51 +0300

    clean up tests

--- opensrp-chw-malaria/src/test/java/org/smartregister/activity/BaseMalariaProfileActivityTest.java
+++ opensrp-chw-malaria/src/test/java/org/smartregister/activity/BaseMalariaProfileActivityTest.java
@@ -1,25 +1,15 @@
 package org.smartregister.activity;
 
-import android.view.View;
-
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import org.mockito.Spy;
 import org.smartregister.chw.malaria.activity.BaseMalariaProfileActivity;
 
 public class BaseMalariaProfileActivityTest {
     @Mock
     protected BaseMalariaProfileActivity baseMalariaProfileActivity;
-
-    @Spy
-    protected BaseMalariaProfileActivity spy;
-
-    @Mock
-    private View view;
 
     @Before
     public void setUp() {
@@ -31,10 +21,4 @@ public class BaseMalariaProfileActivityTest {
         Assert.assertNotNull(baseMalariaProfileActivity);
     }
 
-    @Test
-    public void hideView() {
-        Mockito.doNothing().when(spy).hideView();
-        spy.hideView();
-        Mockito.verify(view).setVisibility(View.GONE);
-    }
 }
