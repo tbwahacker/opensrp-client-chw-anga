bd7594b0bdec5819367d7f6c860d56a26ae42033
Denis Rwelamila <rwelamila.jr@gmail.com>
2019-09-04 12:38:28 +0300

    test BaseMalariaFloatingMenu

--- opensrp-chw-malaria/src/test/java/org/smartregister/chw/malaria/custom_views/BaseMalariaFloatingMenuTest.java
+++ opensrp-chw-malaria/src/test/java/org/smartregister/chw/malaria/custom_views/BaseMalariaFloatingMenuTest.java
@@ -6,36 +6,32 @@ import org.junit.Test;
 import org.mockito.Mockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.rule.PowerMockRule;
-
 @PrepareForTest(BaseMalariaFloatingMenu.class)
 public class BaseMalariaFloatingMenuTest {
     @Rule
     public PowerMockRule rule = new PowerMockRule();
 
     @Test
-    public void BaseMalariaFloatingMenu() {
-        BaseMalariaFloatingMenu baseMalariaFloatingMenu = Mockito.spy(new BaseMalariaFloatingMenu(null, null));
-
-        // do your actual tests here
-        Mockito.verify(baseMalariaFloatingMenu).initUi();
-    }
-
-    @Test
     public void getPhoneNumber() {
         BaseMalariaFloatingMenu baseMalariaFloatingMenu = Mockito.spy(new BaseMalariaFloatingMenu(null, "John", "0744112211", "Doe", "0744112244"));
-
         Assert.assertEquals("0744112211", baseMalariaFloatingMenu.getPhoneNumber());
     }
 
     @Test
     public void getFamilyHeadName() {
+        BaseMalariaFloatingMenu baseMalariaFloatingMenu = Mockito.spy(new BaseMalariaFloatingMenu(null, "John", "0744112211", "Doe", "0744112244"));
+        Assert.assertEquals("Doe", baseMalariaFloatingMenu.getFamilyHeadName());
     }
 
     @Test
     public void getFamilyHeadPhone() {
+        BaseMalariaFloatingMenu baseMalariaFloatingMenu = Mockito.spy(new BaseMalariaFloatingMenu(null, "John", "0744112211", "Doe", "0744112244"));
+        Assert.assertEquals("0744112244", baseMalariaFloatingMenu.getFamilyHeadPhone());
     }
 
     @Test
     public void getClientName() {
+        BaseMalariaFloatingMenu baseMalariaFloatingMenu = Mockito.spy(new BaseMalariaFloatingMenu(null, "John", "0744112211", "Doe", "0744112244"));
+        Assert.assertEquals("John", baseMalariaFloatingMenu.getClientName());
     }
 }
