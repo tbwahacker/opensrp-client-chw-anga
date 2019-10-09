7681cd74e7a098c5143e0f6448ab34db81feb6d1
Denis R <rwelamila.jr@gmail.com>
2019-10-09 11:36:39 +0300

    clean ups

--- opensrp-chw-malaria/src/test/java/org/smartregister/activity/BaseMalariaProfileActivityTest.java
+++ opensrp-chw-malaria/src/test/java/org/smartregister/activity/BaseMalariaProfileActivityTest.java
@@ -1,26 +1,15 @@
 package org.smartregister.activity;
 
-import android.graphics.Color;
-import android.view.View;
-
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import org.mockito.Spy;
 import org.smartregister.chw.malaria.activity.BaseMalariaProfileActivity;
 
 public class BaseMalariaProfileActivityTest {
     @Mock
     protected BaseMalariaProfileActivity baseMalariaProfileActivity;
-
-    @Spy
-    protected BaseMalariaProfileActivity spy;
-
-    @Mock
-    protected View view;
 
     @Before
     public void setUp() {
@@ -32,21 +21,4 @@ public class BaseMalariaProfileActivityTest {
         Assert.assertNotNull(baseMalariaProfileActivity);
     }
 
-    @Test
-    public void setProfileViewWithData() {
-        baseMalariaProfileActivity.setProfileViewWithData();
-        Mockito.verify(view, Mockito.never()).setVisibility(View.VISIBLE);
-    }
-
-    @Test
-    public void setDueColor() {
-        baseMalariaProfileActivity.setDueColor();
-        Mockito.verify(view, Mockito.never()).setBackgroundColor(Color.BLUE);
-    }
-
-    @Test
-    public void setOverDueColor() {
-        baseMalariaProfileActivity.setOverDueColor();
-        Mockito.verify(view, Mockito.never()).setBackgroundColor(Color.RED);
-    }
 }
