64228777e0ca287a8d2d89a440335c5177db76f3
Denis R <rwelamila.jr@gmail.com>
2019-10-10 15:33:27 +0300

    more tests for BaseMalariaRegisterPresenter

--- opensrp-chw-malaria/src/test/java/org/smartregister/presenter/BaseMalariaRegisterPresenterTest.java
+++ opensrp-chw-malaria/src/test/java/org/smartregister/presenter/BaseMalariaRegisterPresenterTest.java
@@ -12,46 +12,56 @@ import org.powermock.reflect.Whitebox;
 import org.smartregister.chw.malaria.contract.MalariaRegisterContract;
 import org.smartregister.chw.malaria.presenter.BaseMalariaRegisterPresenter;
 
+
 @PrepareForTest(BaseMalariaRegisterPresenter.class)
 public class BaseMalariaRegisterPresenterTest {
     @Mock
     protected BaseMalariaRegisterPresenter baseMalariaRegisterPresenter;
 
     @Mock
+    protected MalariaRegisterContract.Interactor interactor;
+    @Mock
+    protected MalariaRegisterContract.Model model;
+    @Mock
     protected MalariaRegisterContract.View baseView;
+    private BaseMalariaRegisterPresenter presenter;
 
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
+        presenter = new BaseMalariaRegisterPresenter(baseView, model, interactor);
     }
 
     @Test
     public void startFormWhenEntityIdIsNull() throws Exception {
-        baseMalariaRegisterPresenter.startForm("formName", "", "121212121212", "231231231231");
+        presenter.startForm("formName", "", "121212121212", "231231231231");
         Mockito.verify(baseView, Mockito.never()).startFormActivity(null);
     }
 
-//    @Test
-//    public void startFormWhenEntityIdIsNotNull() throws Exception {
-//        baseMalariaRegisterPresenter.startForm("formName", "12131212", "121212121212", "231231231231");
-//        PowerMockito.verifyPrivate(baseMalariaRegisterPresenter).invoke("getView");
-//    }
-
     @Test
-    public void startFormWithEntityId() throws Exception {
-        baseMalariaRegisterPresenter.startForm("formName", "12121212", "121212121212", "231231231231");
-        Mockito.verify(baseView, Mockito.never()).startFormActivity(new JSONObject());
+    public void startFormWhenEntityIdIsNotNull() throws Exception {
+        JSONObject form = model.getFormAsJson("formName", "12131212", "231231231231");
+        Mockito.when(model.getFormAsJson("formName", "12131212", "231231231231")).thenReturn(form);
+        Mockito.doNothing().when(baseView).startFormActivity(form);
+        presenter.startForm("formName", "12131212", "121212121212", "231231231231");
+        Mockito.verify(baseView).startFormActivity(form);
     }
 
-//    @Test
-//    public void saveForm() {
-//        baseMalariaRegisterPresenter.saveForm("{}");
-//        Mockito.verify(baseView).showProgressDialog(1);
-//    }
+    @Test
+    public void saveForm() {
+        presenter.saveForm("{}");
+        Mockito.verify(interactor).saveRegistration("{}", presenter);
+    }
 
     @Test
     public void getViewWhenViewIsNull() throws Exception {
         Assert.assertNull(Whitebox.invokeMethod(baseMalariaRegisterPresenter, "getView"));
+    }
+
+    @Test
+    public void onRegistrationSaved() {
+        presenter.onRegistrationSaved();
+        Mockito.verify(baseView).hideProgressDialog();
     }
 
 }
