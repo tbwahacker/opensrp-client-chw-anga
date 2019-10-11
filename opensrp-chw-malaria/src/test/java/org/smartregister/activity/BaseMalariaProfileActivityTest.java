a4d75261295aa51858f6fe468bb81b567f11ec2d
Denis R <rwelamila.jr@gmail.com>
2019-10-11 10:17:42 +0300

    clean BaseMalariaProfileActivityTest

--- opensrp-chw-malaria/src/test/java/org/smartregister/activity/BaseMalariaProfileActivityTest.java
+++ opensrp-chw-malaria/src/test/java/org/smartregister/activity/BaseMalariaProfileActivityTest.java
@@ -1,15 +1,22 @@
 package org.smartregister.activity;
 
+import android.graphics.Color;
+import android.view.View;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.smartregister.chw.malaria.activity.BaseMalariaProfileActivity;
 
 public class BaseMalariaProfileActivityTest {
     @Mock
     protected BaseMalariaProfileActivity baseMalariaProfileActivity;
+
+    @Mock
+    protected View view;
 
     @Before
     public void setUp() {
