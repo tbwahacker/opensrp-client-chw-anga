dcc577f0adb26dcda0491c5ad925472eba4e7958
JafariUCC <jafarimwidadi@gmail.com>
2019-11-12 14:20:18 +0300

    provider tests

--- opensrp-chw-malaria/src/test/java/org/smartregister/chw/malaria/provider/MalariaRegisterProviderTest.java
+++ opensrp-chw-malaria/src/test/java/org/smartregister/chw/malaria/provider/MalariaRegisterProviderTest.java
@@ -5,6 +5,8 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.powermock.reflect.Whitebox;
+import org.smartregister.chw.malaria.activity.BaseMalariaProfileActivity;
 
 public class MalariaRegisterProviderTest {
     @Mock
@@ -30,4 +32,20 @@ public class MalariaRegisterProviderTest {
     public void checkInflater() {
         Assert.assertNull(malariaRegisterProvider.inflater());
     }
+
+    @Test
+    public void checkFooter() {
+        Assert.assertNotNull(malariaRegisterProvider.isFooterViewHolder(null));
+    }
+
+    @Test
+    public void checkFooterCreation() {
+        Assert.assertNull(malariaRegisterProvider.createFooterHolder(null));
+    }
+
+    @Test
+    public void checkHolderCreation() {
+        Assert.assertNull(malariaRegisterProvider.createViewHolder(null));
+    }
+
 }
