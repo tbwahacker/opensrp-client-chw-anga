cf78b3cc9938778d9786965575a3c0517a17f220
Manu Tarus <manutarus@gmail.com>
2019-11-15 12:25:07 +0300

    process malaria confirmation form update

--- opensrp-chw-malaria/src/main/java/org/smartregister/chw/malaria/interactor/BaseMalariaRegisterInteractor.java
+++ opensrp-chw-malaria/src/main/java/org/smartregister/chw/malaria/interactor/BaseMalariaRegisterInteractor.java
@@ -2,13 +2,9 @@ package org.smartregister.chw.malaria.interactor;
 
 import android.support.annotation.VisibleForTesting;
 
-import org.smartregister.chw.malaria.MalariaLibrary;
 import org.smartregister.chw.malaria.contract.MalariaRegisterContract;
 import org.smartregister.chw.malaria.util.AppExecutors;
-import org.smartregister.chw.malaria.util.JsonFormUtils;
 import org.smartregister.chw.malaria.util.Util;
-import org.smartregister.clientandeventmodel.Event;
-import org.smartregister.repository.AllSharedPreferences;
 
 public class BaseMalariaRegisterInteractor implements MalariaRegisterContract.Interactor {
 
@@ -28,7 +24,7 @@ public class BaseMalariaRegisterInteractor implements MalariaRegisterContract.In
 
         Runnable runnable = () -> {
             try {
-                saveRegistration(jsonString);
+                Util.saveFormEvent(jsonString);
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -36,13 +32,5 @@ public class BaseMalariaRegisterInteractor implements MalariaRegisterContract.In
             appExecutors.mainThread().execute(() -> callBack.onRegistrationSaved());
         };
         appExecutors.diskIO().execute(runnable);
-    }
-
-    private void saveRegistration(final String jsonString) throws Exception {
-
-        AllSharedPreferences allSharedPreferences = MalariaLibrary.getInstance().context().allSharedPreferences();
-        Event baseEvent = JsonFormUtils.processJsonForm(allSharedPreferences, jsonString);
-
-        Util.processEvent(allSharedPreferences, baseEvent);
     }
 }
