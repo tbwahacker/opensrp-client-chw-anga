d9b67071aee70615dc06bc63803e05667b22fa31
Manu Tarus <manutarus@gmail.com>
2020-03-05 14:22:43 +0300

    remane jsonutils class

--- opensrp-chw-malaria/src/main/java/org/smartregister/chw/malaria/model/BaseMalariaRegisterModel.java
+++ opensrp-chw-malaria/src/main/java/org/smartregister/chw/malaria/model/BaseMalariaRegisterModel.java
@@ -2,14 +2,14 @@ package org.smartregister.chw.malaria.model;
 
 import org.json.JSONObject;
 import org.smartregister.chw.malaria.contract.MalariaRegisterContract;
-import org.smartregister.chw.malaria.util.JsonFormUtils;
+import org.smartregister.chw.malaria.util.MalariaJsonFormUtils;
 
 public class BaseMalariaRegisterModel implements MalariaRegisterContract.Model {
 
     @Override
     public JSONObject getFormAsJson(String formName, String entityId, String currentLocationId) throws Exception {
-        JSONObject jsonObject = JsonFormUtils.getFormAsJson(formName);
-        JsonFormUtils.getRegistrationForm(jsonObject, entityId, currentLocationId);
+        JSONObject jsonObject = MalariaJsonFormUtils.getFormAsJson(formName);
+        MalariaJsonFormUtils.getRegistrationForm(jsonObject, entityId, currentLocationId);
 
         return jsonObject;
     }
