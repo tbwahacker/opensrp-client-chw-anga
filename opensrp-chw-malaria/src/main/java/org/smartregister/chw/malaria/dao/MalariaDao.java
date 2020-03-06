c468da1081bc2cdbc0d889b04fafcb7d65e1f5b7
Manu Tarus <manutarus@gmail.com>
2020-03-06 13:45:30 +0300

    add function to count family member on malaria register

--- opensrp-chw-malaria/src/main/java/org/smartregister/chw/malaria/dao/MalariaDao.java
+++ opensrp-chw-malaria/src/main/java/org/smartregister/chw/malaria/dao/MalariaDao.java
@@ -53,6 +53,20 @@ public class MalariaDao extends AbstractDao {
         return res.get(0) > 0;
     }
 
+    public static Integer getMalariaFamilyMembersCount(String familyBaseEntityId) {
+        String sql = "SELECT count(emc.base_entity_id) count FROM ec_malaria_confirmation emc " +
+                "INNER Join ec_family_member fm on fm.base_entity_id = emc.base_entity_id " +
+                "WHERE fm.relational_id = '" + familyBaseEntityId + "' AND fm.is_closed = 0 " +
+                "AND emc.is_closed = 0 AND emc.malaria = 1";
+
+        DataMap<Integer> dataMap = cursor -> getCursorIntValue(cursor, "count");
+
+        List<Integer> res = readData(sql, dataMap);
+        if (res == null || res.size() == 0)
+            return 0;
+        return res.get(0);
+    }
+
     public static MemberObject getMember(String baseEntityID) {
         String sql = "select m.base_entity_id , m.unique_id , m.relational_id , m.dob , m.first_name , m.middle_name , m.last_name , m.gender , m.phone_number , m.other_phone_number , f.first_name family_name ,f.primary_caregiver , f.family_head , f.village_town ,fh.first_name family_head_first_name , fh.middle_name family_head_middle_name , fh.last_name family_head_last_name, fh.phone_number family_head_phone_number , ancr.is_closed anc_is_closed, pncr.is_closed pnc_is_closed, pcg.first_name pcg_first_name , pcg.last_name pcg_last_name , pcg.middle_name pcg_middle_name , pcg.phone_number  pcg_phone_number , mr.* from ec_family_member m inner join ec_family f on m.relational_id = f.base_entity_id inner join ec_malaria_confirmation mr on mr.base_entity_id = m.base_entity_id left join ec_family_member fh on fh.base_entity_id = f.family_head left join ec_family_member pcg on pcg.base_entity_id = f.primary_caregiver left join ec_anc_register ancr on ancr.base_entity_id = m.base_entity_id left join ec_pregnancy_outcome pncr on pncr.base_entity_id = m.base_entity_id where m.base_entity_id ='" + baseEntityID + "' ";
         SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault());
